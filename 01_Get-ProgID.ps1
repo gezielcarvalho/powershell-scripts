35d32c0999365b371c193a25d217dc6b47be197b
Geziel Carvalho <geziel.natal@gmail.com>
2024-11-07 20:02:38 -0500

    Add script to connect to SQL Server and retrieve server name

---
+++ 01_Get-ProgID.ps1
@@ -0,0 +1,36 @@
+﻿<#
+.SYNOPSIS            
+	Gets all of the ProgIDs registered on a system
+.DESCRIPTION            
+	Gets all ProgIDs registered on the system.  The ProgIDs returned can be used with New-Object -comObject
+.EXAMPLE
+	Get-ProgID
+	
+	Description
+	-----------
+	Reads all ProgIDs from the registry on the local system
+.EXAMPLE
+	Get-ProgID | Where-Object { $_.ProgID -like "*ado*" }
+	
+	Description
+	-----------
+	Reads all ProgIDs from the registry on the local system and filters on the name. Only ProgIDs for *ADO* are
+	returned.
+#>
+param()            
+
+$paths = @("REGISTRY::HKEY_CLASSES_ROOT\CLSID")
+if ($env:Processor_Architecture -eq "amd64") {
+    $paths+="REGISTRY::HKEY_CLASSES_ROOT\Wow6432Node\CLSID"
+}
+
+Get-ChildItem $paths -Include VersionIndependentPROGID -Recurse |
+	Select-Object -Property `
+		@{ Name = 'ProgID'; Expression = { $_.GetValue("") } },
+		@{ Name = '32Bit'; Expression = { if ($env:Processor_Architecture -eq "amd64") {
+			$_.PSPath.Contains("Wow6432Node")
+        } else {
+            $true
+        }
+    }
+}
---
+++ 02_ConnectSQLNativeObjects.ps1
@@ -0,0 +1,20 @@
+﻿[string] $SqlAuthLoginName = "sa" #$SqlAuthCredential.GetNetworkCredential().UserName
+[string] $SqlAuthPassword = "yourStrong123Password" #$SqlAuthCredential.GetNetworkCredential().Password
+
+[string] $ConnectionString = "Server=localhost;Database=master;User ID=$SqlAuthLoginName;Password=$SqlAuthPassword;Encrypt=false;TrustServerCertificate=False;Connection Timeout=30;"
+
+$Connection = New-Object System.Data.SqlClient.SqlConnection
+$Connection.ConnectionString = $ConnectionString
+$Connection.Open();
+
+
+$Command = $Connection.CreateCommand()
+$Command.CommandText = 
+"
+select @@SERVERNAME
+"
+    
+$reader = $Command.ExecuteReader()
+$reader.HasRows
+$Command.Dispose()
+$Connection.Close()
