848b454069175b45cb370d97dc418c8df7d3d822
Geziel Carvalho <geziel.natal@gmail.com>
2024-11-07 20:15:21 -0500

    Add script to demonstrate object reuse and function calls in PowerShell

---
+++ 04_ObjectReuse.ps1
@@ -0,0 +1,18 @@
+﻿Write-Output "1 - Creating a function that returns an object"
+function a {
+    $value = Get-Item -Path "C:\"
+    $value
+}
+Write-Output "2 - Declaring a variable and assigning a value"
+$Value = "bla"
+Write-Output "3 - Calling the variable"
+$Value
+Write-Output "4 - Calling the function"
+a
+Write-Output "5 - Calling the variable again"
+$Value
+Write-Output "6 - Assigning a new value to the variable"
+$Value = "something else"
+Write-Output "7 - Calling the variable again"
+$value
+
