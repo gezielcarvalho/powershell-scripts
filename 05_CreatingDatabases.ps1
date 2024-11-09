78d70e21364c780ca3533210301f9c45cadd87a7
Geziel Carvalho <geziel.natal@gmail.com>
2024-11-09 17:59:30 -0500

    Add script to create and drop a database in SQL Server

---
+++ 05_CreatingDatabases.ps1
@@ -0,0 +1,22 @@
+﻿# Navigate to the SQL Server instance path
+
+# Remember to replace `COMPUTER_NAME` with your SQL Server instance name 
+# and `SQLEXPRESS22` with the appropriate instance name.
+
+Set-Location SQLSERVER:\SQL\COMPUTER_NAME\SQLEXPRESS22
+
+# Get the server object
+$srv = Get-Item SQLSERVER:\SQL\COMPUTER_NAME\SQLEXPRESS22
+
+# Create a new database
+$db = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Database -ArgumentList $srv, "Test3"
+$db.Create()
+
+# Reference the database and display the date when it was created.
+$db = $srv.Databases["Test3"]
+$db.CreateDate
+
+# Drop the database
+$db.Drop()
+
+Set-Location C:
