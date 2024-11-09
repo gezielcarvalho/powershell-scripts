2aa7c8fbaa1f7cb33e921030329ee239dc8f240c
Geziel Carvalho <geziel.natal@gmail.com>
2024-11-09 18:43:15 -0500

    Add script to create, alter, and drop a table in SQL Server using SMO

---
+++ 07_CreatingTables.ps1
@@ -0,0 +1,53 @@
+﻿#Load the assembly containing the objects used in this example  
+[reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.Types")  
+  
+# Remember to replace `COMPUTER_NAME` with your SQL Server instance name 
+# and `SQLEXPRESS22` with the appropriate instance name.
+# Set the path context to the local, default instance of SQL Server.  
+Set-Location SQLSERVER:\SQL\COMPUTER_NAME\SQLEXPRESS22\Databases\
+
+#And the database object corresponding to Test3.  
+$db = get-item Test3
+  
+#Create a SMO Table  
+$tb = New-Object -TypeName Microsoft.SqlServer.Management.SMO.Table -argumentlist $db, "Test_Table"  
+  
+#Add various columns to the table.   
+$Type = [Microsoft.SqlServer.Management.SMO.DataType]::NChar(50)  
+$col1 =  New-Object -TypeName Microsoft.SqlServer.Management.SMO.Column -argumentlist $tb,"Name", $Type  
+$col1.Collation = "Latin1_General_CI_AS"  
+$col1.Nullable = $true  
+$tb.Columns.Add($col1)  
+  
+$Type = [Microsoft.SqlServer.Management.SMO.DataType]::Int  
+$col2 =  New-Object -TypeName Microsoft.SqlServer.Management.SMO.Column -argumentlist $tb,"ID", $Type  
+$col2.Identity = $true  
+$col2.IdentitySeed = 1  
+$col2.IdentityIncrement = 1  
+$tb.Columns.Add($col2)   
+  
+$Type = [Microsoft.SqlServer.Management.SMO.DataType]::Real  
+$col3 =  New-Object -TypeName Microsoft.SqlServer.Management.SMO.Column -argumentlist $tb,"Value", $Type  
+$tb.Columns.Add($col3)   
+  
+$Type = [Microsoft.SqlServer.Management.SMO.DataType]::DateTime  
+$col4 =  New-Object -TypeName Microsoft.SqlServer.Management.SMO.Column -argumentlist $tb,"Date", $Type  
+$col4.Nullable = $false  
+$tb.Columns.Add($col4)   
+  
+#Create the table  
+$tb.Create()  
+  
+$Type = [Microsoft.SqlServer.Management.SMO.DataType]::DateTime  
+$col5 =  New-Object -TypeName Microsoft.SqlServer.Management.SMO.Column -argumentlist $tb,"ExpiryDate", $Type  
+$col5.Nullable = $false  
+$tb.Columns.Add($col5)   
+  
+#Run the Alter method to make the change on the instance of SQL Server.   
+$tb.Alter()  
+  
+#Remove the table from the database.   
+$tb.Drop()
+
+# Set the path back to the drive.
+Set-Location C:
